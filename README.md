81a4579291fd1f9eed90e878ef044811ff82f243
Matthias Güntert <matthias.guentert@sidenis.com>
2021-03-09 22:16:41 +0100

    Heavy update on readme, added many new modules

--- README.md
+++ README.md
@@ -19,10 +19,169 @@ However you can modify them according to your needs. The underyling module uses 
 # Important notes
 
 - After assigning an initiative it takes up to 30min until it becomes active!
-
-# Current limitations
-
 - Currently grouping of policies within the initiative is not possible due to a terraform [bug](https://github.com/terraform-providers/terraform-provider-azurerm/issues/10155)
+
+# Currently implemented resources
+
+## General 
+
+|Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Management group | `mg-` |✅||
+| Resource group | `rg-` |✅||
+| Policy definition | `policy-` |✅||
+| API management service instance | `apim-` |✅||
+| Managed Identity | `id-` |✅||
+
+## Networking 
+
+|Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Virtual network | `vnet-` |✅||
+| Subnet | `snet-` |✅||
+| Virtual network peering | `peer-` |✅||
+| Network interface (NIC) | `nic-` |✅||
+| Public IP address | `pip-` |✅||
+| Load balancer (internal) | `lbi-` |🟥|not working yet|
+| Load balancer (external) | `lbe-` |🟥|not working yet|
+| Network security group (NSG) | `nsg-` |✅||
+| Application security group (ASG) | `asg-` |✅||
+| Local network gateway | `lgw-` |✅||
+| Virtual network gateway | `vgw-` |✅||
+| VPN connection | `cn-` |✅||
+| ExpressRoute circuit | `erc-` |✅||
+| Application gateway | `agw-` |✅||
+| Route table | `route-` |✅||
+| User defined route (UDR) | `udr-` |✅||
+| Traffic Manager profile | `traf-` |✅||
+| Front door | `fd-` |✅||
+| CDN profile | `cdnp-` |✅||
+| CDN endpoint | `cdne-` |✅||
+| Web Application Firewall (WAF) policy | `waf` |✅||
+
+## Compute and Web 
+
+Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Virtual machine | `vm` |✅||
+| Virtual machine scale set | `vmss-` |✅||
+| Availability set | `avail-` |✅||
+| Managed disk (OS) | `osdisk` |🟨|wont implement|
+| Managed disk (data) | `disk` |🟨|wont implement|
+| VM storage account | `stvm` |🟨|wont implement|
+| Azure Arc enabled server | `arcs-` |🟨|wont implement|
+| Azure Arc enabled Kubernetes cluster | `arck` |🟨|wont implement|
+| Container registry | `cr` |✅||
+| Container instance | `ci-` |
+| AKS cluster | `aks-` |
+| Service Fabric cluster | `sf-` |
+| App Service environment | `ase-` |
+| App Service plan | `plan-` |
+| Web app | `app-` |
+| Static web app | `stapp` |
+| Function app | `func-` |
+| Cloud service | `cld-` |🟨|wont implement|
+| Notification Hubs | `ntf-` |
+| Notification Hubs namespace | `ntfns-` |
+
+## Databases 
+
+Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Azure SQL Database server | `sql-` |
+| Azure SQL database | `sqldb-` |
+| Azure Cosmos DB database | `cosmos-` |
+| Azure Cache for Redis instance | `redis-` |
+| MySQL database | `mysql-` |
+| PostgreSQL database | `psql-` |
+| Azure SQL Data Warehouse | `sqldw-` |
+| Azure Synapse Analytics | `syn-` |
+| SQL Server Stretch Database | `sqlstrdb-` |
+| SQL Managed Instance | `sqlmi-` |
+
+## Storage 
+
+Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Storage account | `st` |✅||
+| Azure StorSimple | `ssimp` |
+| Azure Container Registry | `acr` |
+
+## AI and Machine Learning
+
+Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Azure Cognitive Search | `srch-` |
+| Azure Cognitive Services | `cog-` |
+| Azure Machine Learning workspace | `mlw-` |
+
+## Analytics and IoT
+
+Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Azure Analysis Services server | `as` |
+| Azure Databricks workspace | `dbw-` |
+| Azure Stream Analytics | `asa-` |
+| Azure Data Explorer cluster | `dec` |
+| Azure Data Factory | `adf-` |
+| Data Lake Store account | `dls` |
+| Data Lake Analytics account | `dla` |
+| Event Hubs namespace | `evhns-` |
+| Event hub | `evh-` |
+| Event Grid domain | `evgd-` |
+| Event Grid topic | `evgt-` |
+| HDInsight - Hadoop cluster | `hadoop-` |
+| HDInsight - HBase cluster | `hbase-` |
+| HDInsight - Kafka cluster | `kafka-` |
+| HDInsight - Spark cluster | `spark-` |
+| HDInsight - Storm cluster | `storm-` |
+| HDInsight - ML Services cluster | `mls-` |
+| IoT hub | `iot-` |
+| Power BI Embedded | `pbi-` |
+| Time Series Insights environment | `tsi-` |
+
+## Developer tools
+
+Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| App Configuration store | `appcs-` |
+| Azure Static Web Apps | `stap-` |
+
+## Integration
+
+Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Integration account | `ia-` |
+| Logic apps | `logic-` |
+| Service Bus | `sb-` |
+| Service Bus queue | `sbq-` |
+| Service Bus topic | `sbt-` |
+
+## Management and governance
+
+Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Automation account | `aa-` |
+| Azure Monitor action group | `ag-` |
+| Azure Purview instance | `pview-` |
+| Blueprint | `bp-` |
+| Blueprint assignment | `bpa-` |
+| Key vault | `kv-` |
+| Log Analytics workspace | `log-` |
+| Application Insights | `appi-` |
+
+## Migration
+
+Asset type | Abbreviation | Status | Comment 
+|--|--|--|--|
+| Azure Migrate project | `migr-` |
+| Database Migration Service instance | `dms-` |
+| Recovery Services vault | `rsv-` |
+
+# Ideas for the future
+
+- Create a bicep version 
+- Create a .NET version using Azure SDK 
 
 # Further reading 
 
@@ -30,3 +189,4 @@ However you can modify them according to your needs. The underyling module uses 
 - [azurerm_policy_definition](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/policy_definition)
 - [azurerm_policy_set_definition](https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/policy_set_definition#policy_definition_group)
 - [Resource providers for Azure services](https://docs.microsoft.com/en-us/azure/azure-resource-manager/management/azure-services-resource-providers)
+- [Azure Templates](https://docs.microsoft.com/en-us/azure/templates/)
--- main.tf
+++ main.tf
@@ -19,16 +19,13 @@ resource "azurerm_policy_set_definition" "initiative" {
     display_name = "Naming Convention"
     description = "Naming convention initiative following Microsoft's recommendation"
 
+    # General 
     policy_definition_reference {
       policy_definition_id = module.management-group-policy.id
     }
 
     policy_definition_reference {
       policy_definition_id = module.resource-group-policy.id
-    }
-
-    policy_definition_reference {
-      policy_definition_id = module.storage-account-policy.id 
     }
 
     policy_definition_reference {
@@ -39,12 +36,26 @@ resource "azurerm_policy_set_definition" "initiative" {
       policy_definition_id = module.api-management-service-policy.id
     }
 
-        policy_definition_reference {
+    policy_definition_reference {
       policy_definition_id = module.managed-identity-policy.id
     }
+
+    # Networking 
+    # policy_definition_reference {
+    #   policy_definition_id = module.virtual-network-policy.id
+    # }    
+
+    # Storage
+    policy_definition_reference {
+      policy_definition_id = module.storage-account-policy.id 
+    }
+
+    # policy_definition_reference {
+    #   policy_definition_id = module.xxxxx.id
+    # }
 }
 
-### General
+# General
 
 module "management-group-policy" {
     source = "./policy-module"
@@ -86,8 +97,233 @@ module "managed-identity-policy" {
     display_name = "Managed Identities"
 }
 
+# Networking 
 
-### Storage
+module "virtual-network-policy" {
+    source = "./policy-module"
+
+    pattern = "vnet-*"
+    type = "Microsoft.Network/virtualNetworks"
+    display_name = "Virtual Networks"
+}
+
+module "subnet-policy" {
+    source = "./policy-module"
+
+    pattern = "snet-*"
+    type = "Microsoft.Network/virtualNetworks/subnets"
+    display_name = "Subnets"
+}
+
+module "virtual-network-peering-policy" {
+    source = "./policy-module"
+
+    pattern = "peer-*"
+    type = "Microsoft.Network/virtualNetworks/virtualNetworkPeerings"
+    display_name = "Virtual Network Peerings"
+}
+
+module "network-interface-policy" {
+    source = "./policy-module"
+
+    pattern = "nic-*"
+    type = "Microsoft.Network/networkInterfaces"
+    display_name = "Network Interfaces"
+}
+
+module "public-ip-address-policy" {
+    source = "./policy-module"
+
+    pattern = "pip-*"
+    type = "Microsoft.Network/publicIPAddresses"
+    display_name = "Public IP Addresses"
+}
+
+# TODO: Create custom module > lb is internal if pip is missing
+# resources.properties.frontendIPConfigurations.properites.publicIPAddress does not exist
+module "load-balancer-internal-policy" {
+    source = "./policy-module"
+
+    pattern = "lbi-*"
+    type = "Microsoft.Network/loadBalancers"
+    display_name = "Internal Load Balancers"
+}
+
+# TODO: Create custom module > lb is external if pip is attached
+# resources.properties.frontendIPConfigurations.properites.publicIPAddress not null
+module "load-balancer-external-policy" {
+    source = "./policy-module"
+
+    pattern = "lbe-*"
+    type = "Microsoft.Network/loadBalancers"
+    display_name = "External Load Balancers"    
+}
+
+module "network-security-group-policy" {
+    source = "./policy-module"
+
+    pattern = "nsg-*"
+    type = "Microsoft.Network/networkSecurityGroups"
+    display_name = "Network Security Groups"       
+}
+
+module "application-security-group-policy" {
+    source = "./policy-module"
+
+    pattern = "asg-*"
+    type = "Microsoft.Network/applicationSecurityGroups"
+    display_name = "Application Security Groups"  
+}
+
+module "local-network-gateway-policy" {
+    source = "./policy-module"
+
+    pattern = "lgw-*"
+    type = "Microsoft.Network/localNetworkGateways"
+    display_name = "Local Network Gateways"  
+}
+
+module "virtual-network-gateway-policy" {
+    source = "./policy-module"
+
+    pattern = "vgw-*"
+    type = "Microsoft.Network/virtualNetworkGateways"
+    display_name = "Virtual Network Gateways"  
+}
+
+module "vpn-connection-policy" {
+    source = "./policy-module"
+
+    pattern = "cn-*"
+    type = "Microsoft.Network/vpnGateways/vpnConnections"
+    display_name = "VPN Connections"  
+}
+
+module "express-route-circuit-policy" {
+    source = "./policy-module"
+
+    pattern = "erc-*"
+    type = "Microsoft.Network/expressRouteCircuits"
+    display_name = "Express Route Circuits"  
+}
+
+module "application-gateway-policy" {
+    source = "./policy-module"
+
+    pattern = "agw-*"
+    type = "Microsoft.Network/applicationGateways"
+    display_name = "Application Gateways"  
+}
+
+module "route-table-policy" {
+    source = "./policy-module"
+
+    pattern = "route-*"
+    type = "Microsoft.Network/routeTables"
+    display_name = "Route Tables"  
+}
+
+module "user-defined-route-policy" {
+    source = "./policy-module"
+
+    pattern = "udr-*"
+    type = "Microsoft.Network/routeTables/routes"
+    display_name = "User Defined Routes"  
+}
+
+module "traffic-manager-profile-policy" {
+    source = "./policy-module"
+
+    pattern = "traf-*"
+    type = "Microsoft.Network/trafficmanagerprofiles"
+    display_name = "Traffic Manager Profiles"  
+}
+
+module "front-door-policy" {
+    source = "./policy-module"
+
+    pattern = "fd-*"
+    type = "icrosoft.Network/frontDoors"
+    display_name = "Front Doors"  
+}
+
+# Todo: Move together into custom module cdn-endpoint-policy
+module "cdn-profile-policy" {
+    source = "./policy-module"
+
+    pattern = "cdnp-*"
+    type = "Microsoft.Cdn/profiles"
+    display_name = "CDN Profiles"  
+}
+
+# Todo: Move together into custom module cdn-profile-policy
+module "cdn-endpoint-policy" {
+    source = "./policy-module"
+
+    pattern = "cdne-*"
+    type = "microsoft.cdn/profiles/endpoints"
+    display_name = "CDN Endpoints"  
+}
+
+module "waf-policy" {
+    source = "./policy-module"
+
+    pattern = "waf*"
+    type = "Microsoft.Network/ApplicationGatewayWebApplicationFirewallPolicies"
+    display_name = "Web Application Firewalls"  
+}
+
+# Compute and Web 
+
+module "virtual-machine-policy" {
+    source = "./policy-module"
+
+    pattern = "vm*"
+    type = "Microsoft.Compute/virtualMachines"
+    display_name = "Virtual Machines"  
+}
+
+module "virtual-machine-scale-set-policy" {
+    source = "./policy-module"
+
+    pattern = "vmss-*"
+    type = "Microsoft.Compute/virtualMachineScaleSets"
+    display_name = "Virtual Machine Scale Sets"  
+}
+
+module "availability-set-policy" {
+    source = "./policy-module"
+
+    pattern = "avail-*"
+    type = "Microsoft.Compute/availabilitySets"
+    display_name = "Availability Sets"  
+}
+
+module "container-registry-policy" {
+    source = "./policy-module"
+
+    pattern = "cr*"
+    type = "Microsoft.ContainerRegistry/registries"
+    display_name = "Container Registries"  
+}
+
+module "xxxx-policy" {
+    source = "./policy-module"
+
+    pattern = "xxx*"
+    type = "xxx"
+    display_name = "xxx"  
+}
+
+module "xxxx-policy" {
+    source = "./policy-module"
+
+    pattern = "xxx*"
+    type = "xxx"
+    display_name = "xxx"  
+}
+
+# Storage
 
 module "storage-account-policy" {
     source = "./policy-module"
